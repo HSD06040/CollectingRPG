70d9b7097e52d2c68702244547a63ee0e412c7d6
HSD06040 <tinto0604@gmail.com>
2025-08-14 18:07:51 +0900

    [Chore] imports 폴더 생성 및 gitignore

---
+++ Assets/Workspace/HSD/Scripts/Unit/UnitStats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitBaseData : ScriptableObject
+{
+    public int MaxHealth;
+    public int Damage;
+}
