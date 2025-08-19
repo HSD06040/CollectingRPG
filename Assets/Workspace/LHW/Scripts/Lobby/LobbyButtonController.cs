74fed41161d70d8a495e465fe8b474530f567924
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-08-19 15:42:06 +0900

    [Feat] 로비 UI 하단 버튼 상호작용 완성

---
+++ Assets/Workspace/LHW/Scripts/Lobby/LobbyButtonController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyButtonController : MonoBehaviour
+{
+    [SerializeField] private Button _shopButton;
+    [SerializeField] private Button _upgradeButton;
+    [SerializeField] private Button _stageButton;
+    [SerializeField] private Button _characterCompositionButton;
+    [SerializeField] private Button _gachaButton;
+
+    [SerializeField] private GameObject _shopPanel;
+    [SerializeField] private GameObject _upgradePanel;
+    [SerializeField] private GameObject _stagePanel;
+    [SerializeField] private GameObject _characterCompositionPanel;
+    [SerializeField] private GameObject _gachaPanel;
+
+    private void Awake()
+    {
+        _shopButton.onClick.AddListener(ShopOpen);
+        _upgradeButton.onClick.AddListener(UpgradeOpen);
+        _stageButton.onClick.AddListener(StageOpen);
+        _characterCompositionButton.onClick.AddListener(CharacterCompositionOpen);
+        _gachaButton.onClick.AddListener(GachaOpen);
+
+        StageOpen();
+    }
+
+    private void ShopOpen()
+    {
+        SetActivePanel("ShopPanel");
+    }
+
+    private void UpgradeOpen()
+    {
+        SetActivePanel("UpgradeUIPanel");
+    }
+
+    private void StageOpen()
+    {
+        SetActivePanel("StageUIPanel");
+    }
+
+    private void CharacterCompositionOpen()
+    {
+        SetActivePanel("CharacterCompositionUIPanel");
+    }
+
+    private void GachaOpen()
+    {
+        SetActivePanel("GachaUIPanel");
+    }
+
+    private void SetActivePanel(string activePanel)
+    {
+        _shopPanel.SetActive(activePanel.Equals(_shopPanel.name));
+        _upgradePanel.SetActive(activePanel.Equals(_upgradePanel.name));
+        _stagePanel.SetActive(activePanel.Equals(_stagePanel.name));
+        _characterCompositionPanel.SetActive(activePanel.Equals(_characterCompositionPanel.name));
+        _gachaPanel.SetActive(activePanel.Equals(_gachaPanel.name));
+    }
+}
