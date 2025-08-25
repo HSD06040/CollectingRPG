665bac94f6edb5842c61b17e4ce9abddb85e9a94
99jinmo <99jinmo@gmail.com>
2025-08-25 17:42:25 +0900

    [Feat] 가챠 연출

---
+++ Assets/Workspace/JJM/Scripts/GachaEffectTestStarter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GachaEffectTestStarter : MonoBehaviour
+{
+    public GachaEffectController gachaEffectController;
+    public UnitData testUnitData;
+
+    public ProfileIconInfiniteScroll infiniteScroll;
+    public PlayerInventory playerInventory;
+
+    void Start()
+    {
+        // 씬 시작 시 자동 테스트
+        gachaEffectController.RequestGachaEffect(testUnitData);
+        infiniteScroll.SetDataList(playerInventory.ownedUnits);
+    }
+}
