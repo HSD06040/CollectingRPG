707789d9372cfdeb9a9053bca84ef6b92db6f54a
YeonHouu <yeonhhou@gmail.com>
2025-08-26 17:52:26 +0900

    [Chore] Scenes In Build 변경
    개인 테스트 씬 -> 기본 씬으로 변경

--- Assets/Workspace/CYH/Scripts/Firebase/GuestSignIn.cs
+++ Assets/Workspace/CYH/Scripts/Firebase/GuestSignIn.cs
@@ -77,8 +77,7 @@ public class GuestSignIn : MonoBehaviour
                 // 튜토리얼 isTutorialComplete = true Data 변경
                 SetTutorialCompleteAsync();
                 _isClicked = false;
-                SceneManager.LoadScene("LobbyScene_Copy");
-                SceneManager.LoadScene("LobbyScene_Copy");
+                SceneManager.LoadScene("USW_LobbyScene");
             }
         });
     }
@@ -88,11 +87,11 @@ public class GuestSignIn : MonoBehaviour
         bool isTutorialCompleted = await Manager.DB.CheckTutorialCompletedAsync();
         if (isTutorialCompleted)
         {
-            SceneManager.LoadScene("LobbyScene_Copy");
+            SceneManager.LoadScene("USW_LobbyScene");
         }
         else
         {
-            SceneManager.LoadScene("LobbyScene_Copy");
+            SceneManager.LoadScene("USW_LobbyScene");
             // TODO: [CYH] 패널 전환 테스트_2 (삭제 예정)
             //tutorialPanel.SetActive(true);
             //SigninPanel.SetActive(false);
