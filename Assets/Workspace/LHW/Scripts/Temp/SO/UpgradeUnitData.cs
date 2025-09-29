ba9c785da450afd684c8b4d1c28f7ed4a712bea6
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-09-29 16:07:52 +0900

    [Feat] 캐릭터 신화석을 정해진 비율만큼 소모하여 강화하는 데 추가 사용 구현

--- Assets/Workspace/LHW/Scripts/Temp/SO/UpgradeUnitData.cs
+++ Assets/Workspace/LHW/Scripts/Temp/SO/UpgradeUnitData.cs
@@ -88,7 +88,7 @@ public class UpgradeUnitData : ScriptableObject
 
         if (CurrentUpgradeData.UpgradeLevel == 0)
         {
-            if(CurrentUpgradeData.CurrentPieces >= 10)
+            if (CurrentUpgradeData.CurrentPieces >= 10)
             {
                 CurrentUpgradeData.CurrentPieces -= 10;
                 CurrentUpgradeData.UpgradeLevel += 1;
@@ -102,9 +102,11 @@ public class UpgradeUnitData : ScriptableObject
             int requiredGold = GetRequiredGold();
 
             string uid = FirebaseManager.Auth.CurrentUser.UserId;
-            var diaRef = FirebaseManager.DataReference.Child("UserData").Child(uid).Child("Gold");
+            var goldRef = FirebaseManager.DataReference.Child("UserData").Child(uid).Child("Gold");
+            var mythStoneRef = FirebaseManager.DataReference.Child("UserData").Child(uid).Child("MythStone");
 
-            DataSnapshot snapshot = await diaRef.GetValueAsync();
+            DataSnapshot snapshot = await goldRef.GetValueAsync();
+            DataSnapshot snapshot2 = await mythStoneRef.GetValueAsync();
 
             int currentGold = 0;
 
@@ -112,6 +114,15 @@ public class UpgradeUnitData : ScriptableObject
             {
                 currentGold = Convert.ToInt32(snapshot.Value);
             }
+
+            int currentMythStone = 0;
+
+            if (snapshot2.Exists && snapshot2.Value != null)
+            {
+                currentMythStone = Convert.ToInt32(snapshot2.Value);
+            }
+
+            int conversedMythstone = currentMythStone / MythStonePieceRatio(_grade);
 
             if (CurrentUpgradeData.CurrentPieces >= requiredPiece && currentGold >= requiredGold)
             {
@@ -121,7 +132,33 @@ public class UpgradeUnitData : ScriptableObject
 
                 OnLevelUp?.Invoke();
             }
+            else if (CurrentUpgradeData.CurrentPieces + conversedMythstone >= requiredPiece && currentGold >= requiredGold)
+            {
+                int subMythStone = requiredPiece - CurrentUpgradeData.CurrentPieces;
+                await DBManager.Instance.SubtractMythStoneAsync(subMythStone * MythStonePieceRatio(_grade));
+
+                CurrentUpgradeData.CurrentPieces = 0;
+                CurrentUpgradeData.UpgradeLevel += 1;
+                await DBManager.Instance.SubtractGoldAsync(requiredGold);
+
+                OnLevelUp?.Invoke();
+            }
         }
+    }
+
+    private int MythStonePieceRatio(Grade grade)
+    {
+        int pieceRatio = 0;
+
+        switch (grade)
+        {
+            case Grade.NORMAL: pieceRatio = 2; break;
+            case Grade.RARE: pieceRatio = 4; break;
+            case Grade.UNIQUE: pieceRatio = 6; break;
+            case Grade.LEGEND: pieceRatio = 9; break;
+        }
+
+        return pieceRatio;
     }
 }
 
